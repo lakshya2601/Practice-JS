9c4d1b90451ac2abd132dc2959cfe13c40920d97
Lakshya Porwal <89459982+lakshya2601@users.noreply.github.com>
2025-02-15 13:14:58 +0530

    refactor App component: update API endpoint and clean up unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,23 @@
-
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect,  } from 'react';
 
 const App = () => {
 
-const[data, setData]= useState([])
 
-useEffect(() => {
-  fetch("https://raw.githubusercontent.com/Vibencode-Solutions/mock-api/refs/heads/main/api.json")
-  .then((response)=> response.json())
-  .then((data)=>setData(data))
-  .catch((error)=> console.log(error))
-}, []);
+  useEffect(() => {
+    fetch("https://api.jsonbin.io/v3/b/67b03680acd3cb34a8e2dec8")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-const childData=(nodes)=>{
-  return  nodes.map((node, index)=>(
-    <li key={index}>
-      {node.name}
-      {node.child.length > 0 && <ul>{childData(node.child)}</ul>}
-    </li>
-  ));
-
-};
-
+  
 
 
   return (
     <div>
-      <ul>{childData(data)}</ul>
+      
     </div>
-  )
+  );
 }
 
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App;
